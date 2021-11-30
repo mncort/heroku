19ab9c222590805f4df661e81fd0780b7e8cb814
Mariano Cortina <mncort@users.noreply.github.com>
2021-11-30 00:17:20 -0300

    3 entrega 2

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express")
 const Contenedor = require('./contenedor')
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 const app = express()
-const PORT = 3000 
+
+
+const server = app.listen(server_port, server_host, () => {
+    console.log(`El servidor esta escuchando en el puerto: ${server.address().port}`)
+    console.log(`La 'base de datos' cargo con exito ${contenedor.getAll().length} registros`)
+})
+
+server.on("error", error => console.log(`El servidor ha sufrido un error ${error}`))
+
 let archivo = new Contenedor("test.JSON")
-
-let test = app.listen(PORT)
-
-test.on('error', error => console.log)
-
 
 app.get('/productos', (request, response) => {
     archivo.getAll()
